5bdf7a8a6d3bfa81b2fbfed57b00208122ea292a
Reshetov Yuriy <ryv1983@mail.ru>
2024-03-11 19:48:54 +0300

    3 Variant for the task of Resheto Eratrothena works out

--- 4_Challange_prime_numbers/prime_numbers.c
+++ 4_Challange_prime_numbers/prime_numbers.c
@@ -1,44 +1,80 @@
-#include <math.h>
 #include <stdio.h>
 
-int isPrime(int num)
-{
-    double sq = sqrt(num);
-    for(int i = 2; i <= sq; i++) {
-        if(num % i == 0)
-            return 0;
-    }
-    if(num == 1)
-        return 0;
-    return 1;
-}
+//++++++++++++++++++++++++++ 1-2 VARIANT +++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-int main(int argc, char** argv)
+//#include <math.h>
+
+// int isPrime(int num)
+//{
+//     double sq = sqrt(num);
+//     for(int i = 2; i <= sq; i++) {
+//         if(num % i == 0)
+//             return 0;
+//     }
+//     if(num == 1)
+//         return 0;
+//     return 1;
+// }
+//
+// int main(int argc, char** argv)
+//{
+//     int primes[100] = { 0 };
+//     printf("%d\n", primes[10]);
+//     int next = 2;
+//     for(int j = 0; primes[j-1] < 100; j++) {
+//     // 1 -ый вариант с циклом while()
+//         while(primes[99] == 0) {
+//             if(isPrime(next)) {
+//                 primes[j] = next;
+//                 next++;
+//                 break;
+//             }
+//             next++;
+//         }
+//     }
+//     // 2-ой вариант с циклом for()
+//     //        for(int i = next; i < 10000; i++) {
+//     //            if(isPrime(i)) {
+//     //                primes[j] = i;
+//     //                next = i + 1;
+//     //                break;
+//     //            }
+//     //        }
+//     //
+//     for(int j = 0; primes[j]<100; j++)
+//         printf("%d : %d\n", j, primes[j]);
+//     return 0;
+// }
+
+//++++++++++++++++++++++++++ 1-2 VARIANT +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+int main()
 {
-    int primes[100] = { 0 };
-    printf("%d\n", primes[10]);
-    int next = 2;
-    for(int j = 0; j < 100; j++) {
-    // 1 -ый вариант с циклом while()
-        while(primes[99] == 0) {
-            if(isPrime(next)) {
-                primes[j] = next;
-                next++;
-                break;
-            } 
-            next++;
+    int p;
+    int i;
+    int primes[50] = { 0 };
+    primes[0] = 2;
+    primes[1] = 3;
+
+    int PrimeIndex = 2;
+
+    _Bool isPrime = 1;
+
+    for(p = 5; p < 100; p+=2) {
+        isPrime = 1;
+        for(i = 1; isPrime && p / primes[i] >= primes[i]; ++i) {
+                if(p%primes[i]==0) {
+                    isPrime = 0;
+                    break;
+                }
+        }
+        if(isPrime) {
+        primes[PrimeIndex] = p;
+        PrimeIndex++;
         }
     }
-    // 2-ой вариант с циклом for()
-    //        for(int i = next; i < 10000; i++) {
-    //            if(isPrime(i)) {
-    //                primes[j] = i;
-    //                next = i + 1;
-    //                break;
-    //            }
-    //        }
-    //    
-    for(int j = 0; j < 100; j++)
-        printf("%d : %d\n", j, primes[j]);
+    
+    for (int i = 0; i<PrimeIndex; i++) printf("%d : %d\n", i, primes[i]);
+    
     return 0;
 }
