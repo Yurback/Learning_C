806e3952f6db3b88780f0538cd86e806ae8b9379
Reshetov Yuriy <ryv1983@mail.ru>
2024-03-12 15:34:51 +0300

    gcd more short realisation

--- 6_Functions_sqrt_cgd_abs/main.c
+++ 6_Functions_sqrt_cgd_abs/main.c
@@ -1,37 +1,49 @@
 #include <stdio.h>
 
 // 1-st part
-int isPrime(int num)
-{
-    if((num % 2 == 0 && num != 2) || num <= 1)
-        return 0;
-    for(int i = 3; i * i <= num; i += 2) {
-        if(num % i == 0)
-            return 0;
-    }
-    return 1;
-}
-
+//int isPrime(int num)
+//{
+//    if((num % 2 == 0 && num != 2) || num <= 1)
+//        return 0;
+//    for(int i = 3; i * i <= num; i += 2) {
+//        if(num % i == 0)
+//            return 0;
+//    }
+//    return 1;
+//}
+//
+//int gcd(unsigned num1, unsigned num2)
+//{
+//    int gcd = 1;
+//    num1 == num2 ? gcd = num1 : num1 % num2 == 0 ? gcd = num2 : num2 % num1 == 0 ? gcd = num1 : 0;
+//    if(gcd != 1)
+//        return gcd;
+//    int min;
+//    int max = (int)num1 - (int)num2 > 0 ? num1 : num2;
+//    min = max == num1 ? num2 : num1;
+//    for(int d = 2; d * d <= max; d++) {
+//        if(max % d == 0) {
+//            if(min % d == 0 && d > gcd)
+//                gcd = d;
+//            int great_denum = max / d;
+//            if(great_denum > d && num1 % great_denum == 0 && num2 % great_denum == 0)
+//                gcd = great_denum;
+//        }
+//    }
+//    return gcd;
+//}
+// 2-variant more short
 int gcd(unsigned num1, unsigned num2)
 {
-    int gcd = 1;
-    num1 == num2 ? gcd = num1 : num1 % num2 == 0 ? gcd = num2 : num2 % num1 == 0 ? gcd = num1 : 0;
-    if(gcd != 1)
-        return gcd;
-    int min;
-    int max = (int)num1 - (int)num2 > 0 ? num1 : num2;
-    min = max == num1 ? num2 : num1;
-    for(int d = 2; d * d <= max; d++) {
-        if(max % d == 0) {
-            if(min % d == 0 && d > gcd)
-                gcd = d;
-            int great_denum = max / d;
-            if(great_denum > d && num1 % great_denum == 0 && num2 % great_denum == 0)
-                gcd = great_denum;
+    int tmp;
+    while(num2 != 0) {
+        tmp = num1%num2;
+        num1=num2;
+        num2=tmp;
         }
-    }
-    return gcd;
+    return num1;
 }
+
 // 2-nd part
 float abs_number(float num)
 {
@@ -40,18 +52,21 @@ float abs_number(float num)
     return check;
 }
 // 3-rd part
-float square_root(float num) { 
+float square_root(float num)
+{
     if(num < 0) {
-        printf("Square_root only takes a positive number");
+        printf("Square_root only takes a positive number\n");
         return -1;
     }
-    float i=1;
-    while((int)(i*i*100) != (int)num*100) {
-        if(i*i<num)i++;
-        if(i*i>num) i-=0.01;
+    float i = 1;
+    while((int)(i * i * 100) != (int)num * 100) {
+        if(i * i < num)
+            i++;
+        if(i * i > num)
+            i -= 0.01;
     }
     return i;
- }
+}
 
 int main(int argc, char** argv)
 {
@@ -65,11 +80,11 @@ int main(int argc, char** argv)
     //    printf("%d\n", isPrime(7));
     //    printf("%d\n", isPrime(2));
 
-    printf("gcd is %d\n", gcd(12, 8));
+    printf("gcd is %d\n", gcd(4, 6));
 
     printf("%.2f\n", abs_number(55));
-    
-    printf("%.2f\n", square_root(20));
+
+    printf("%.2f\n", square_root(-25));
 
     return 0;
 }
