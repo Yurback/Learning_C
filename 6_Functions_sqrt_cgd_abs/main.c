0cca29b98762f583f4ffe1846614fb56954c48b9
Reshetov Yuriy <ryv1983@mail.ru>
2024-03-12 14:56:26 +0300

    absolute and square number prepared

--- 6_Functions_sqrt_cgd_abs/main.c
+++ 6_Functions_sqrt_cgd_abs/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+// 1-st part
 int isPrime(int num)
 {
-    if(num % 2 == 0 && num != 2 || num <= 1)
+    if((num % 2 == 0 && num != 2) || num <= 1)
         return 0;
     for(int i = 3; i * i <= num; i += 2) {
         if(num % i == 0)
@@ -19,31 +20,56 @@ int gcd(unsigned num1, unsigned num2)
         return gcd;
     int min;
     int max = (int)num1 - (int)num2 > 0 ? num1 : num2;
-    min = max==num1? num2 : num1;
+    min = max == num1 ? num2 : num1;
     for(int d = 2; d * d <= max; d++) {
-        if(max%d==0) {
-            if(min%d==0 && d>gcd)
+        if(max % d == 0) {
+            if(min % d == 0 && d > gcd)
                 gcd = d;
             int great_denum = max / d;
             if(great_denum > d && num1 % great_denum == 0 && num2 % great_denum == 0)
                 gcd = great_denum;
         }
+    }
     return gcd;
 }
+// 2-nd part
+float abs_number(float num)
+{
+    float check = 0;
+    check = num < 0 ? -num : num;
+    return check;
+}
+// 3-rd part
+float square_root(float num) { 
+    if(num < 0) {
+        printf("Square_root only takes a positive number");
+        return -1;
+    }
+    float i=1;
+    while((int)(i*i*100) != (int)num*100) {
+        if(i*i<num)i++;
+        if(i*i>num) i-=0.01;
+    }
+    return i;
+ }
 
 int main(int argc, char** argv)
 {
-    printf("%d\n", isPrime(15));
-    printf("%d\n", isPrime(31));
-    printf("%d\n", isPrime(30));
-    printf("%d\n", isPrime(3));
-    printf("%d\n", isPrime(4));
-    printf("%d\n", isPrime(5));
-    printf("%d\n", isPrime(6));
-    printf("%d\n", isPrime(7));
-    printf("%d\n", isPrime(2));
+    //    printf("%d\n", isPrime(15));
+    //    printf("%d\n", isPrime(31));
+    //    printf("%d\n", isPrime(30));
+    //    printf("%d\n", isPrime(3));
+    //    printf("%d\n", isPrime(4));
+    //    printf("%d\n", isPrime(5));
+    //    printf("%d\n", isPrime(6));
+    //    printf("%d\n", isPrime(7));
+    //    printf("%d\n", isPrime(2));
 
-    printf("gcd is %d\n", gcd(12,8));
+    printf("gcd is %d\n", gcd(12, 8));
+
+    printf("%.2f\n", abs_number(55));
+    
+    printf("%.2f\n", square_root(20));
 
     return 0;
 }
