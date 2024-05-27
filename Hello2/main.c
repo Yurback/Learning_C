ea186af7fe0d977040531d919662c6c46ad48d09
Reshetov Yuriy <ryv1983@mail.ru>
2024-05-27 20:01:31 +0300

    Tiny corrections with misc code

--- Hello2/main.c
+++ Hello2/main.c
@@ -2,6 +2,8 @@
 
 int main(int argc, char **argv)
 {
+    unsigned int x = 0b10000000000000000000000000000000; // 2^31
+    printf("x is %u\n", x);
     printf("%ld byte\n", sizeof(size_t));
 	printf("hello world\n");
 	return 0;
--- Hello_world/main.c
+++ Hello_world/main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
 int main(int argc, char **argv)
 {
-    int x = 10;
-    int y = 8;
-    int z = 2;
+    char x = 10;
+    int y = 0;
+    char z = 1;
     
-    int arr[] = {0,[2]=x, [10]=85, [2]=20};
+    char arr[] = {0,[2]=x, [10]=85, [2]=20, [5]=z};
+    
+    arr[y]=10;
+    
+    for(char i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
+
+        printf("%d,", arr[(int)i]);
+    }
     
     printf("size of arr is %ld\n", sizeof(arr)/sizeof(arr[0]));
     
@@ -18,21 +26,22 @@ int main(int argc, char **argv)
     printf("Globe_one is %d\n", GLOB_ONE);
     printf("Globe_two is %d\n", GLOB_TWO);
     printf("Globe_two is %d\n", GLOB_ONE | GLOB_TWO);
-    
+//    
     printf("size is %ld\n", sizeof(void*));
-    
+    printf("size long int is %ld\n", sizeof(long int));
+    printf("size long int is %ld\n", sizeof(long));
     char *word2;
-    word2 = calloc(10,sizeof(char));
+    word2 = calloc(12,sizeof(char));
     word2 =strcpy(word2, "Hello world");
 	// check2 = bfromcstr(word2);
 	printf("CHAR %c\n", *word2);
 	word2[0] = 'g';
     printf("CHAR %c\n", *word2);
-	
-   auto char *word3 = "Hello world";
+//	
+    char *word3 = "Hello world";
 	// check2 = bfromcstr(word2);
 	printf("CHAR %c\n", *word3);
-	word3[0] = 'g';
+//	word3[0] = 'g';                     // CONSTANT STRING CAN'T CHANGE this!!!!!!!!!!!!!
     
     return 0;
 }
